6383583c2a2c100d5916a3b9cc6f00132b50dffb
Leon Tillberg <112480830+LeonTillberg@users.noreply.github.com>
2023-01-24 11:52:48 +0100

    html element och js eventlistener

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-i
+i// JS FÖR FEATURES/SIDA:
+
+const btn = document.querySelector("#submit-btn");
+btn.addEventListener("click", postText);
+
+function postText(event){
+    event.preventDefault();
+    const inputPost = document.querySelector("#user-message");
+    const userMessage = inputPost.value;
+    // document.querySelector("#message-container").innerText = userMessage;
+
+    const divs = document.createElement('div');
+    document.querySelector("#message-container").append(divs);
+    divs.append(userMessage);
+
+    // displayPost(userMessage);
+}
+
+// function displayPost(){
+//     const divs = document.createElement('div');
+//     document.querySelector("#message-container").append(divs);
+//     divs.append(userMessage);
+// }
+
+
+// Submit popup:
+// function submitConfirm(){
+// 	alert("Thank you for your submition!")
+// }
